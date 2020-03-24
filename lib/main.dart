43037d298d3fae897b01bb916fadb2d3a696dc9d
Maycon Moraes <maycondm@Maycons-MacBook-Air.local>
2020-03-24 12:10:59 -0300

    Exemplo de Column

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Frases do Dia",
-    home: Container(
-      color: Colors.white,
-      child: Text("Ola mundo"),
-    ),
+    home: Column(
+      children: <Widget>[
+        Text("Um valor "),
+        Text("Fica em "),
+        Text("em baixo "),
+        Text("de outro ")
+      ],
+    )
   ));
 }
