b0bbbd3299c01eebd1338e918f15feff3febd821
Maycon Moraes <maycondm@Maycons-MacBook-Air.local>
2020-03-24 11:56:40 -0300

    Estrutura minima de um app

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Frases do Dia",
+    home: Container(
+      color: Colors.white,
+      child: Text("Ola mundo"),
+    ),
+  ));
+}
