e2e176fcb17424cb5c7d521aa22895405e269e7d
Maycon de Moraes <maycondm@Maycons-MacBook-Air.local>
2020-03-24 12:13:53 -0300

    Exemplo com row - um valor ao lado do outro

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Frases do Dia",
-    home: Column(
+    home: Row(
       children: <Widget>[
         Text("Um valor "),
         Text("Fica em "),
-        Text("em baixo "),
+        Text("do lado do "),
         Text("de outro ")
       ],
     )
